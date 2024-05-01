71aff053c8459951c2c8ea450bac249e069f0bc8
Sai Ashirwad <saiashirwadr@gmail.com>
2024-05-02 02:12:28 +0530

    types? lua?

--- lua/utils.lua
+++ lua/utils.lua
@@ -6,6 +6,11 @@ M.lazy_plugin = function(plugin)
   return plugin
 end
 
+---@class Keymap
+---@field [1] 'n' | 'i' | 'v'
+---@field [2] string
+---@field [3] string | fun()
+---@param keymaps Keymap[]
 M.set_keymaps = function(keymaps)
   for _, keymap in ipairs(keymaps) do
     vim.keymap.set(keymap[1], keymap[2], keymap[3], {
